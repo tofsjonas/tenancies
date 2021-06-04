f71f07666072e33d941e495c60cbbe89c5ea1d0f
Jonas Earendel <tofsjonas@users.noreply.github.com>
2021-06-05 01:10:24 +0200

    eslint/prettier initialised w typescript support

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-uses-vars': 1,
+    'prettier/prettier': 'error',
+    'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    camelcase: [0],
+    'react/prop-types': [0],
+    'no-unreachable': 'warn',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+}
